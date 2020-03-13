46986ebceafd025c397deccea23067fe10854a48
RichardEsquivel <rlesquivel@gmail.com>
2020-03-13 11:19:05 -0500

    Fixed typo in hyphen compy.py test where there was a comman and modified age range test to tuple instead of list of strings format.

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -54,24 +54,29 @@ print(d)
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
+# utlize f strings to print out formatted string literals easily using loop iteration through list
+e = [f"{human.name}-{human.age}" for human in humans]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
+# Using f string as above with range to provide a range of numbers.
 print("Names and ages between 27 and 32:")
-f = []
+f = [(human.name, human.age) for human in humans if human.age in range(27, 33)]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
+# create a new list of class Human modifying list humans values using upper and +5
 print("All names uppercase:")
-g = []
+g = [Human(human.name.upper(), (human.age + 5)) for human in humans]
 print(g)
+# original humans list is umodified
+print(humans)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
-h = []
+h = [math.sqrt(human.age) for human in humans]
 print(h)
